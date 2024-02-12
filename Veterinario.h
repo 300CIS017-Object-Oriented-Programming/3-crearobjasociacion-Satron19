8ae8842a10b3ce4fb7501f56ada1e24f492b3a4b
Satron19 <118403111+Satron19@users.noreply.github.com>
2024-02-11 20:33:11 -0500

    Create Veterinario.h

---
+++ Veterinario.h
@@ -0,0 +1,27 @@
+/*
+
+Nombre: Samuel David Troncoso
+Profesor: Gonzalo Noreña 
+*/
+
+
+#ifndef VETERINARIO_H
+#define VETERINARIO_H
+
+#include <string>
+
+using namespace std;
+
+class Veterinario{
+
+    string nombre;
+    int aniosExperiencia;
+
+
+    public:
+      Veterinario();
+      Veterinario(string nombre, int aniosExperiencia);
+      void setName(string nombre);
+      void setAniosExperiencia(int anos);
+      string getName();
+      int getAniosExperiancia();
