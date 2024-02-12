0e76b0440cc321bc28c77c428cd30c010b42b298
Satron19 <118403111+Satron19@users.noreply.github.com>
2024-02-11 20:09:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,13 +18,18 @@ int main() {
 
     //Agregar un nuevo pPropietario a Firulais
     firulais.agregarPropietario("Carlos Zapata", "102842313");
-    pepper.agregarPropietario("Jaime Bond","101243542");
+    
     Propietario* juan = new Propietario(123456789);
     juan->setNombre("Juan");
     juan->setEdad(30);
+
+    pepper.agregarPropietario(juan);
     
 
     // Consultar el nombre del pPropietario del perro Firulais
+
+    cout << juan->mostrarInfo() << endl;
+    
     std::cout<< "El nombre del pPropietario del perro "<<firulais.getNombre() << " es " << firulais.getPropietario()->getNombre()<<std::endl;
 
     return 0;
